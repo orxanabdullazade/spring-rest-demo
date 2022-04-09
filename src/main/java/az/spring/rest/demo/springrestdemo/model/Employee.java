550f61b920d5ad078127495f9e456de6b3c4495d
orxanabdullazade <orxanzadeh@gmail.com>
2022-04-09 22:01:46 +0400

    first commit

---
+++ src/main/java/az/spring/rest/demo/springrestdemo/model/Employee.java
@@ -0,0 +1,11 @@
+package az.spring.rest.demo.springrestdemo.model;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "employee")
+public class Employee {
+
+
+}
