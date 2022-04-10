929567d2f9ae65f2da1b79aefba18e1e2237cf27
orxanabdullazade <orxanzadeh@gmail.com>
2022-04-10 23:08:44 +0400

    open api added

--- src/main/java/az/spring/rest/demo/springrestdemo/SpringRestDemoApplication.java
+++ src/main/java/az/spring/rest/demo/springrestdemo/SpringRestDemoApplication.java
@@ -1,9 +1,18 @@
 package az.spring.rest.demo.springrestdemo;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Employee service api",
+                description = "Employee crud service",
+                version = "v1"
+        )
+)
 public class SpringRestDemoApplication {
 
     public static void main(String[] args) {
--- src/main/java/az/spring/rest/demo/springrestdemo/controller/EmployeeController.java
+++ src/main/java/az/spring/rest/demo/springrestdemo/controller/EmployeeController.java
@@ -3,12 +3,18 @@ package az.spring.rest.demo.springrestdemo.controller;
 import az.spring.rest.demo.springrestdemo.rest.model.dto.EmployeeDto;
 import az.spring.rest.demo.springrestdemo.rest.model.response.EmployeeResponse;
 import az.spring.rest.demo.springrestdemo.service.EmployeeService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
+
+import java.lang.annotation.Target;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/employees")
+@Tag(name = "Employee service")
 public class EmployeeController {
 
     public final EmployeeService employeeService;
@@ -19,6 +25,7 @@ public class EmployeeController {
     }
 
     @GetMapping("/{employee-id}")
+    @Operation(summary = "This gets employee by id")
     public EmployeeDto getEmployee(@PathVariable("employee-id") long id){
         return employeeService.getEmployee(id);
     }
